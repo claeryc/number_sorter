4e0a29686632984f9c447949717f22397614d4a6
claeryc <reeko.it@gmail.com>
2025-07-15 15:20:57 +0800

    added insertion-, selection-, bubble-sort functions

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
-updateUI(inputValues);
+  const sortedValues = insertionSort(inputValues);
+//  much simpler (only for single digit values):
+//  const sortedValues = inputValues.sort();
+//  multiple digits:
+//  const sortedValues = inputValues.sort((a, b) => {
+// return a-b;
+// });
+
+    updateUI(sortedValues);
 }
 
 const updateUI = (array = []) => {
@@ -15,6 +23,52 @@ const updateUI = (array = []) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
     outputValueNode.innerText = num;
   })
+}
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+}
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array;
+}
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+  return array;
 }
 
 sortButton.addEventListener("click", sortInputArray);
